463abaeb0f530f5d0cef578397da282f8e6a3c31
Bryant <bshea2k@gmail.com>
2026-04-07 17:00:56 -0700

    Add render and renderSelected functionality to Rectangle class

--- script.js
+++ script.js
@@ -148,8 +148,8 @@ class Shape {
 class Rectangle extends Shape {
     constructor(x, y) {
         super(x, y);
-        this._height = 120;
-        this._width = 80;
+        this._width = 120;
+        this._height = 80;
         this._text = "Text";
     }
 
@@ -160,4 +160,41 @@ class Rectangle extends Shape {
     set height(height) { this._height = height; }
     set width(width) { this._width = width; }
     set text(text) { this._text = text; }
+
+    render(ctx) {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+
+        ctx.font = "16px Helvetica";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "#0D0D0D"
+        ctx.fillText(this.text, this.x + (this.width / 2), this.y + (this.height / 2));
+    }
+
+    renderSelected(ctx) {
+        ctx.fillStyle = "#C9B4F1";
+        ctx.strokeStyle = "#0D0D0D";
+
+        ctx.beginPath();
+        ctx.arc(this.x + this.width / 2, this.y, 5, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(this.x + this.width, this.y + this.height / 2, 5, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(this.x + this.width / 2, this.y + this.height, 5, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(this.x, this.y + this.height / 2, 5, 0, Math.PI * 2, true);
+        ctx.fill();
+        ctx.stroke();
+    }
 }
