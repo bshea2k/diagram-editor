49b1daf749c2cc07e96270af71a43e72a6a5e9cf
Bryant <bshea2k@gmail.com>
2026-04-20 20:42:06 -0700

    Add shapeCreationController class that handles adding shapes to canvas

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,10 +1,7 @@
-import { getElementPosition, getMousePosition } from "./utils";
-import type { Shape } from "./shape";
-import { Rectangle } from "./rectangle.js";
-import { Circle } from "./circle";
 import { Diagram } from "./diagram";
 import { Renderer } from "./renderer";
 import { CanvasController } from "./canvasController";
+import { ShapeCreationController } from "./shapeCreationController";
 
 const canvas: HTMLCanvasElement = document.querySelector("#workspace")!; // IMPROVE NONNULL ASSERTION
 const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
@@ -12,35 +9,4 @@ const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
 const diagram = new Diagram();
 const renderer = new Renderer(ctx);
 const canvasController = new CanvasController(canvas, diagram, renderer);
-let selectedShape: Shape | null = null;
-let draggingShape: boolean = false;
-
-// create rectangle when clicked on in creation menu
-const rect = document.querySelector("#create__rect");
-if (rect) {
-    rect.addEventListener("click", () => {
-        // to be in center, canvas.width / 2 - (shape.width / 2)
-        const rect = new Rectangle(canvas.width / 2, canvas.height / 2);
-        rect.x -= rect.width / 2;
-        rect.y -= rect.height / 2;
-
-        diagram.addShape(rect);
-        selectedShape = rect;
-        
-        renderer.render(diagram, selectedShape, draggingShape);
-    });
-}
-
-const circ = document.querySelector("#create__circ");
-if (circ) {
-    circ.addEventListener("click", () => {
-        const circ = new Circle(canvas.width / 2, canvas.height /2);
-        circ.x -= circ.width / 2;
-        circ.y -= circ.height / 2;
-
-        diagram.addShape(circ);
-        selectedShape = circ;
-        
-        renderer.render(diagram, selectedShape, draggingShape);
-    })
-}
+const shapeCreationController = new ShapeCreationController(canvas, canvasController, diagram);
---
+++ src/scripts/shapeCreationController.ts
@@ -0,0 +1,33 @@
+import type { CanvasController } from "./canvasController";
+import type { Diagram } from "./diagram";
+import { Rectangle } from "./rectangle";
+import { Circle } from "./circle";
+
+export class ShapeCreationController {
+    _canvas: HTMLCanvasElement;
+    _canvasController: CanvasController;
+    _diagram: Diagram;
+
+    constructor(canvas: HTMLCanvasElement, canvasController: CanvasController, diagram: Diagram) {
+        this._canvas = canvas;
+        this._canvasController = canvasController;
+        this._diagram = diagram;
+
+        document.querySelector("#create__rect")?.addEventListener("click", () => {
+            // to be in center, canvas.width / 2 - (shape.width / 2)
+            const rect = new Rectangle(this._canvas.width / 2, this._canvas.height / 2);
+            rect.x -= rect.width / 2;
+            rect.y -= rect.height / 2;
+    
+            this._diagram.addShape(rect);
+        })
+
+        document.querySelector("#create__circ")?.addEventListener("click", () => {
+            const circ = new Circle(canvas.width / 2, canvas.height /2);
+            circ.x -= circ.width / 2;
+            circ.y -= circ.height / 2;
+    
+            diagram.addShape(circ);
+        })
+    }
+}
