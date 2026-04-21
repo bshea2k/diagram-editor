cb012b8ab0b065f4dadb9b385cf9c964e9dc49f9
Bryant <bshea2k@gmail.com>
2026-04-20 22:05:10 -0700

    Add connections to diagram model

--- src/scripts/diagram.ts
+++ src/scripts/diagram.ts
@@ -1,7 +1,9 @@
-import type { Shape } from "./shape"
+import type { Shape } from "./shape";
+import type { Connection } from "./connection";
 
 export class Diagram {
     _shapes: Shape[] = [];
+    _connections: Connection[] = [];
 
     addShape(shape: Shape): void {
         this._shapes.unshift(shape);
@@ -9,5 +11,13 @@ export class Diagram {
 
     getShapes(): Shape[] {
         return this._shapes;
+    }
+
+    addConncetion(connection: Connection): void {
+        this._connections.unshift(connection);
+    }
+
+    getConnections(): Connection[] {
+        return this._connections;
     }
 }
