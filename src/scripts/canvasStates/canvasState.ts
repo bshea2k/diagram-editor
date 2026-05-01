b1d8bb0e653c615880fcab625601a3fc260ed079
Bryant <bshea2k@gmail.com>
2026-05-01 11:23:49 -0700

    Add a state pattern interface for canvas states

---
+++ src/scripts/canvasStates/canvasState.ts
@@ -0,0 +1,10 @@
+import type { CanvasController } from "../canvasController";
+import type { Input } from "../utils";
+
+export abstract class CanvasState {
+    constructor(public canvasController: CanvasController) {}
+
+    abstract enter(input: Input): void;
+
+    abstract handleInput(input: Input): void;
+}
