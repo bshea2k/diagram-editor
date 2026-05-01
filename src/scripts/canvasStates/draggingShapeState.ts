dd77fab5048b131c03fc54363d4840326dbf182c
Bryant <bshea2k@gmail.com>
2026-05-01 14:21:11 -0700

    Add DraggingShape state

---
+++ src/scripts/canvasStates/draggingShapeState.ts
@@ -0,0 +1,19 @@
+import { CanvasState } from "./canvasState";
+import type { CanvasController } from "../canvasController";
+import type { Input } from "../utils";
+
+export class DraggingShapeState extends CanvasState {
+    constructor(canvasController: CanvasController) {
+        super(canvasController);
+    }
+
+    enter(input?: Input): void {
+        this.canvasController.render();
+
+        console.log("Entering DraggingShape"); // temp | debug
+    }
+
+    handleInput(input: Input): void {
+
+    }
+}
