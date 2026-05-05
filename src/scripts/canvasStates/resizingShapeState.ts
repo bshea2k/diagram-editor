46e0d0f208f26f95c893a83a7a7df1c971d8ccc9
Bryant <bshea2k@gmail.com>
2026-05-05 10:50:46 -0700

    Initialize ResizingShape state class with proper variables

--- src/scripts/canvasStates/resizingShapeState.ts
+++ src/scripts/canvasStates/resizingShapeState.ts
@@ -1,19 +1,28 @@
 import { CanvasState } from "./canvasState";
 import type { CanvasController } from "../canvasController";
 import type { Input } from "../utils";
+import type { UtilityPoint } from "../utilityPoints/utilityPoint";
+import type { ResizePoint } from "../utilityPoints/resizePoint";
+import type { ResizeEdge } from "../utilityPoints/resizeEdge";
 
 export class ResizingShapeState extends CanvasState {
-    constructor(canvasController: CanvasController) {
+    private resizeUtilityPoint: ResizePoint | ResizeEdge;
+
+    constructor(canvasController: CanvasController, resizeUtilityPoint: UtilityPoint) {
         super(canvasController);
+
+        if (resizeUtilityPoint.type === "ResizePoint") this.resizeUtilityPoint = resizeUtilityPoint as ResizePoint;
+        else this.resizeUtilityPoint = resizeUtilityPoint as ResizeEdge;
     }
 
     enter(input?: Input): void {
+        this.canvasController.draggingMouse = true;
         this.canvasController.render();
 
         console.log("Entering ResizingShape"); // temp | debug
     }
 
     handleInput(input: Input): void {
-        
+
     }
 }
