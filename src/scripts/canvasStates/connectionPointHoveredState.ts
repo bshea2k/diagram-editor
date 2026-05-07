a7730e22da494c3a1849210ccce0f7246d84b3a2
Bryant <bshea2k@gmail.com>
2026-05-07 14:11:39 -0700

    Enter state properly

--- src/scripts/canvasStates/connectionPointHoveredState.ts
+++ src/scripts/canvasStates/connectionPointHoveredState.ts
@@ -1,14 +1,22 @@
 import { CanvasState } from "./canvasState";
 import type { CanvasController } from "../canvasController";
 import type { Input } from "../utils";
+import type { UtilityPoint } from "../utilityPoints/utilityPoint";
+import type { ConnectionPoint } from "../utilityPoints/ConnectionPoint";
 
 export class ConnectionPointHoveredState extends CanvasState {
-    constructor(canvasController: CanvasController) {
+    private connectionPoint: ConnectionPoint;
+
+    constructor(canvasController: CanvasController, connectionPoint: UtilityPoint) {
         super(canvasController);
+        this.connectionPoint = connectionPoint as ConnectionPoint;
     }
 
     enter(input?: Input): void {
+        this.canvasController.activeUP = this.connectionPoint;
         this.canvasController.render();
+
+        document.body.style.cursor = "crosshair";
 
         console.log("Entering ConnectionPointHovered"); // temp | debug
     }
