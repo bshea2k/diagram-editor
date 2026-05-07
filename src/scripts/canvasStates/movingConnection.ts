0967779dd810c7fb578bd246314792c21dbe183f
Bryant <bshea2k@gmail.com>
2026-05-07 14:27:51 -0700

    Create MovingConnection State

---
+++ src/scripts/canvasStates/movingConnection.ts
@@ -0,0 +1,19 @@
+import { CanvasState } from "./canvasState";
+import type { CanvasController } from "../canvasController";
+import type { Input } from "../utils";
+
+export class MovingConnectionState extends CanvasState {
+    constructor(canvasController: CanvasController) {
+        super(canvasController);
+    }
+
+    enter(input?: Input): void {
+        this.canvasController.render();
+
+        console.log("Entering MovingConnection"); // temp | debug
+    }
+
+    handleInput(input: Input): void {
+
+    }
+}
