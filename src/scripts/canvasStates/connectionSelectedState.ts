158737bf43b5ecc6261b5d190f5206b7f4aa78ee
Bryant <bshea2k@gmail.com>
2026-05-08 18:36:38 -0700

    Go to NothingSelected state when clicking blankspace

--- src/scripts/canvasStates/connectionSelectedState.ts
+++ src/scripts/canvasStates/connectionSelectedState.ts
@@ -1,4 +1,5 @@
 import { CanvasState } from "./canvasState";
+import { NothingSelectedState } from "./nothingSelectedState";
 import type { CanvasController } from "../canvasController";
 import type { Input } from "../utils";
 import type { Connection } from "../connection";
@@ -13,11 +14,17 @@ export class ConnectionSelectedState extends CanvasState {
 
     enter(input?: Input): void {
         this.canvasController.render();
+        document.body.style.cursor = "default";
 
         console.log("Entering ConnectionSelected"); // temp | debug
     }
 
     handleInput(input: Input): void {
-
+        if (input.mouseDown && input.mousePos) {
+            let clickedShape = this.canvasController.detectShape(input.mousePos);
+            
+            // click blankspace -> NothingSelected state
+            if (!clickedShape) this.canvasController.setState(new NothingSelectedState(this.canvasController));
+        }
     }
 }
