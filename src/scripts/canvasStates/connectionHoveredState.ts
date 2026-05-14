4131f1d949f33d07ad8737da1b057eb92bd004e7
Bryant <bshea2k@gmail.com>
2026-05-14 16:44:52 -0700

    Transition to NothingSelected when hovering blankspace

--- src/scripts/canvasStates/connectionHoveredState.ts
+++ src/scripts/canvasStates/connectionHoveredState.ts
@@ -1,4 +1,5 @@
 import { CanvasState } from "./canvasState";
+import { NothingSelectedState } from "./nothingSelectedState";
 import type { CanvasController } from "../canvasController";
 import type { Input } from "../utils";
 import type { Connection } from "../connection";
@@ -18,7 +19,12 @@ export class ConnectionHoveredState extends CanvasState {
     }
 
     handleInput(input: Input): void {
+        if (input.mouseMove && input.mousePos) {
+            let hoveredConnection = this.canvasController.detectConnection(input.mousePos);
 
+            // hover blankspace -> NothingSelected state
+            if(!hoveredConnection) this.canvasController.setState(new NothingSelectedState(this.canvasController));
+        }
     }
 
     exit(input?: Input): void {
