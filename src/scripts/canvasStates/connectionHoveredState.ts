1bb3eb496f238876e5aebf65a4cf37cf5dc8c3ea
Bryant <bshea2k@gmail.com>
2026-05-14 14:53:35 -0700

    Add ConnectionHovered state class

---
+++ src/scripts/canvasStates/connectionHoveredState.ts
@@ -0,0 +1,23 @@
+import { CanvasState } from "./canvasState";
+import type { CanvasController } from "../canvasController";
+import type { Input } from "../utils";
+
+export class NothingSelectedState extends CanvasState {
+    constructor(canvasController: CanvasController) {
+        super(canvasController);
+    }
+
+    enter(input?: Input): void {
+        this.canvasController.render();
+
+        console.log("Entering ConnectionHovered"); // temp | debug
+    }
+
+    handleInput(input: Input): void {
+
+    }
+
+    exit(input?: Input): void {
+        
+    }
+}
