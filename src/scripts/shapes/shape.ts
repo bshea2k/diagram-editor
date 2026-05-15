fcb1a41c4d86ad9467f0ff7bb4f89946b70c7cd9
Bryant <bshea2k@gmail.com>
2026-05-15 14:05:57 -0700

    Update class variables & comments

--- src/scripts/shapes/shape.ts
+++ src/scripts/shapes/shape.ts
@@ -7,19 +7,19 @@ import type { Coord } from "../utils";
 export const SELECTED_POINT_OFFSET = 20;
 
 export abstract class Shape {
-    _id: string;
-    _x: number;
-    _y: number;
-    _width: number;
-    _height: number;
+    public id: string;
+    public x: number;
+    public y: number;
+    public width: number;
+    public height: number;
     public utilityPoints: UtilityPoint[];
 
     constructor(x: number, y: number, width: number, height: number) {
-        this._id = self.crypto.randomUUID();
-        this._x = x;
-        this._y = y;
-        this._width = width;
-        this._height = height;
+        this.id = self.crypto.randomUUID();
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
         this.utilityPoints = [
             new ResizePoint(this, "topleft"),
             new ResizePoint(this, "topright"),
@@ -36,22 +36,12 @@ export abstract class Shape {
         ];
     }
 
-    get id() { return this._id; }
-    get x() { return this._x; }
-    get y() { return this._y; }
-    get height() { return this._height; }
-    get width() { return this._width; }
-
-    set x(x) { this._x = x; }
-    set y(y) { this._y = y; }
-    set height(height) { this._height = height; }
-    set width(width) { this._width = width; }
-
     // renders the shape
     abstract render(ctx: CanvasRenderingContext2D): void;
 
     // returns true if x & y are within the shapes area, false otherwise
     abstract detect(x: number, y: number): boolean;
 
+    // returns the coordinate of the closest point of the edge of the shape
     abstract getNearestEdgePoint(x: number, y: number): Coord;
 }
