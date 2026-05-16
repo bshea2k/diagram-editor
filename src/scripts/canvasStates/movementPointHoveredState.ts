5f5b7c7dd6ccc21456db3b75416d4b09ba221904
Bryant <bshea2k@gmail.com>
2026-05-16 15:34:42 -0700

    Implement state functionality

--- src/scripts/canvasStates/movementPointHoveredState.ts
+++ src/scripts/canvasStates/movementPointHoveredState.ts
@@ -1,23 +1,50 @@
 import { CanvasState } from "./canvasState";
 import type { CanvasController } from "../canvasController";
 import type { Input } from "../utils";
+import type { UtilityPoint } from "../utilityPoints/utilityPoint";
+import type { MovementPoint } from "../utilityPoints/movementPoint";
+import { ConnectionHoveredState } from "./connectionHoveredState";
+import { MovingConnectionState } from "./movingConnectionState";
 
 export class movementPointHoveredState extends CanvasState {
-    constructor(canvasController: CanvasController) {
+    private movementPoint: MovementPoint;
+
+    constructor(canvasController: CanvasController, movementPoint: UtilityPoint) {
         super(canvasController);
+        this.movementPoint = movementPoint as MovementPoint;
     }
 
     enter(input?: Input): void {
+        this.canvasController.activeUP = this.movementPoint;
         this.canvasController.render();
+
+        document.body.style.cursor = "move";
 
         console.log("Entering MovementPointHovered"); // temp | debug
     }
 
     handleInput(input: Input): void {
+        if (input.mouseMove && input.mousePos) {
+            let hoveredUtilityPoint = null;
+            for (const up of this.movementPoint.connection.utilityPoints) {
+                if (up.detect(input.mousePos)) {
+                    hoveredUtilityPoint = up;
+                    break;
+                }
+            }
 
+            if (!hoveredUtilityPoint) {
+                // hover off movement point -> ConnectionHovered state
+                this.canvasController.setState(new ConnectionHoveredState(this.canvasController, this.movementPoint.connection));
+            }
+        }
+        else if (input.mouseDown) {
+            // click -> MovingConnection state
+            this.canvasController.setState(new MovingConnectionState(this.canvasController, this.movementPoint));
+        }
     }
 
     exit(input?: Input): void {
-        
+        this.canvasController.activeUP = null;
     }
 }
