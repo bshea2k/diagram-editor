111a969f9fb1c389631156476562f72d73249b76
Bryant <bshea2k@gmail.com>
2026-05-16 13:50:28 -0700

    Snap connections to shapes when mouse is close enough

--- src/scripts/canvasStates/movingConnection.ts
+++ src/scripts/canvasStates/movingConnection.ts
@@ -22,11 +22,30 @@ export class MovingConnectionState extends CanvasState {
 
     handleInput(input: Input): void {
         if (input.mouseMove && input.mousePos) {
-            if (this.movementPoint.side === "start") {
-                this.movementPoint.connection.startPos = input.mousePos;
+            let snapped = false;
+
+            for (const shape of this.canvasController._diagram.getShapes()) {
+                let edgePoint = shape.getNearestEdgePoint(input.mousePos.x, input.mousePos.y);
+                let xDist = edgePoint.x - input.mousePos.x;
+                let yDist = edgePoint.y - input.mousePos.y;
+                let dist = Math.sqrt(xDist ** 2 + yDist ** 2);
+
+                // snap leniency = 20
+                if (dist <= 20) {
+                    if (this.movementPoint.side === "start") this.movementPoint.connection.connectStartShape(shape, edgePoint);
+                    else this.movementPoint.connection.connectEndShape(shape, edgePoint);
+                    snapped = true;
+                    break;
+                }
             }
-            else {
-                this.movementPoint.connection.endPos = input.mousePos;
+
+            if (!snapped) {
+                if (this.movementPoint.side === "start") {
+                    this.movementPoint.connection.startPos = input.mousePos;
+                }
+                else {
+                    this.movementPoint.connection.endPos = input.mousePos;
+                }
             }
 
             this.canvasController.render();
