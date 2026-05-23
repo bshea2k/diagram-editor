02e46d38bb8025e6643dc7958652d6404d9b480b
Bryant <bshea2k@gmail.com>
2026-05-23 13:56:34 -0700

    Update function headers to account for canvas translations

--- src/scripts/utilityPoints/utilityPoint.ts
+++ src/scripts/utilityPoints/utilityPoint.ts
@@ -3,9 +3,9 @@ import type { Coord } from "../utils";
 export abstract class UtilityPoint {
     constructor(public type: string) {}
 
-    abstract detect(pos: Coord): boolean;
+    abstract detect(pos: Coord, xOffset: number, yOffset: number): boolean;
 
-    abstract render(ctx: CanvasRenderingContext2D): void;
+    abstract render(ctx: CanvasRenderingContext2D, xOffset: number, yOffset: number): void;
 
-    abstract renderActive(ctx: CanvasRenderingContext2D): void;
+    abstract renderActive(ctx: CanvasRenderingContext2D, xOffset: number, yOffset: number): void;
 }
