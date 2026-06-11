7408a3af67660f7e7c3106659fc63f67d46013c0
Bryant <bshea2k@gmail.com>
2026-06-11 01:17:09 -0700

    Enable functionality for shape text to be bolded

--- src/scripts/toolbarController.ts
+++ src/scripts/toolbarController.ts
@@ -2,6 +2,8 @@ import type { CanvasController } from "./canvasController";
 import type { Connection } from "./connection";
 import type { Shape } from "./shapes/shape";
 
+const boldIcon: Element = document.querySelector("#toolbar__button--bold")!;
+
 export class ToolbarController {
     private canvasController: CanvasController;
     private selectedShape: Shape | null = null;
@@ -9,5 +11,16 @@ export class ToolbarController {
 
     constructor(canvasController: CanvasController) {
         this.canvasController = canvasController;
+
+        boldIcon.addEventListener("click", this.handleBold);
+    }
+
+    handleBold = (): void => {
+        const shape = this.canvasController.selectedShape;
+        
+        if (shape) {
+            shape.bolded = !shape.bolded;
+            this.canvasController.render();
+        }
     }
 }
