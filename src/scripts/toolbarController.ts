1e1fc6e26ca85a86eea97ff0f8f1786f1c24dcbe
Bryant <bshea2k@gmail.com>
2026-06-11 19:27:21 -0700

    Implement functionality to change vertical text alignment

--- src/scripts/toolbarController.ts
+++ src/scripts/toolbarController.ts
@@ -3,6 +3,9 @@ import type { Connection } from "./connection";
 import type { Shape } from "./shapes/shape";
 
 const boldIcon: Element = document.querySelector("#toolbar__button--bold")!;
+const textAlignTopIcon: Element = document.querySelector("#toolbar__button--align-top")!;
+const textAlignCenterIcon: Element = document.querySelector("#toolbar__button--align-center")!;
+const textAlignBottomIcon: Element = document.querySelector("#toolbar__button--align-bottom")!;
 
 export class ToolbarController {
     private canvasController: CanvasController;
@@ -13,13 +16,39 @@ export class ToolbarController {
         this.canvasController = canvasController;
 
         boldIcon.addEventListener("click", this.handleBold);
+        textAlignTopIcon.addEventListener("click", this.handleTextAlignTop);
+        textAlignCenterIcon.addEventListener("click", this.handleTextAlignCenter);
+        textAlignBottomIcon.addEventListener("click", this.handleTextAlignBottom);
     }
 
     handleBold = (): void => {
         const shape = this.canvasController.selectedShape;
-        
         if (shape) {
             shape.bolded = !shape.bolded;
+            this.canvasController.render();
+        }
+    }
+
+    handleTextAlignTop = (): void => {
+        const shape = this.canvasController.selectedShape;
+        if (shape) {
+            shape.textVAlign = "top";
+            this.canvasController.render();
+        }
+    }
+
+    handleTextAlignCenter = (): void => {
+        const shape = this.canvasController.selectedShape;
+        if (shape) {
+            shape.textVAlign = "middle";
+            this.canvasController.render();
+        }
+    }
+
+    handleTextAlignBottom = (): void => {
+        const shape = this.canvasController.selectedShape;
+        if (shape) {
+            shape.textVAlign = "bottom";
             this.canvasController.render();
         }
     }
